b1474049db767519595771c6c65e63136fbf8c33
vishalkanteppa <vishalkanteppa@gmail.com>
2019-06-26 11:52:39 +0530

    Simple chatbot that uses the college handbook as its corpus

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# code obatained from https://medium.com/analytics-vidhya/building-a-simple-chatbot-in-python-using-nltk-7c8c8215ac6e
-
-
 import io
 import random
 import string 
@@ -16,29 +13,23 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 nltk.download('popular', quiet=True) # for downloading packages
 
-nltk.download('punkt') # first-time use only
-nltk.download('wordnet') # first-time use only
-
-
-#Reading in the corpus
-# with open('handbook.txt','r', encoding='utf8', errors ='ignore') as fin:
-#     raw = fin.read().lower()
+nltk.download('punkt') 
+nltk.download('wordnet') 
 
 raw=clean('handbook.txt')
-# print(raw)
+
 #TOkenisation
 sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
-# print(sent_tokens)
+
 # Preprocessing
 lemmer = WordNetLemmatizer()
 def LemTokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
-# print(remove_punct_dict)
+
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
-
 
 # Keyword Matching
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
@@ -53,45 +44,39 @@ def greeting(sentence):
 
 # Generating response
 def response(user_response):
-    robo_response=''
+    response=''
     sent_tokens.append(user_response)
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
-    # print(TfidfVec)
     tfidf = TfidfVec.fit_transform(sent_tokens)
-    # print(tfidf," shape---->", np.shape(tfidf))
     vals = cosine_similarity(tfidf[-1], tfidf)
-    # print(vals," shape---->", np.shape(vals))
     idx=vals.argsort()[0][-2]
     flat = vals.flatten()
     flat.sort()
     req_tfidf = flat[-2]
     if(req_tfidf==0):
-        robo_response=robo_response+"I am sorry! I don't understand you"
-        return robo_response
+        response=response+"I am sorry! I don't understand you"
+        return response
     else:
-        robo_response = robo_response+sent_tokens[idx]
-        return robo_response
+        response = response+sent_tokens[idx]
+        return response
 
 
 flag=True
-print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+print(">>Hi there! If you want to exit, type Bye!")
 while(flag==True):
     user_response = input()
     user_response=user_response.lower()
     if(user_response!='bye'):
         if(user_response=='thanks' or user_response=='thank you' ):
             flag=False
-            print("ROBO: You are welcome..")
+            print(">> You're welcome.")
         else:
             if(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
+                print(">> "+greeting(user_response))
             else:
-                print("ROBO: ",end="")
+                print(">> ",end="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
     else:
         flag=False
-        print("ROBO: Bye! take care..")    
-        
-        
-
+        print(">> Bye! take care..")    
