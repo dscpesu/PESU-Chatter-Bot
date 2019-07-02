6806228272c59550fa1b617a849e184e3c60d76f
vishalkanteppa <51087060+vishalkanteppa@users.noreply.github.com>
2019-07-03 00:37:52 +0530

    Hardcoded chatbot

--- main.py
+++ main.py
@@ -1,82 +1,137 @@
-import io
+import pandas as pd
+import spacy as sp
 import random
-import string 
-import warnings
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import warnings
-from cleanup import clean
-warnings.filterwarnings('ignore')
-
-import nltk 
+import nltk
 from nltk.stem import WordNetLemmatizer
-nltk.download('popular', quiet=True) # for downloading packages
+import datetime
+import os
 
-nltk.download('punkt') 
-nltk.download('wordnet') 
-
-raw=clean('handbook.txt')
-
-#TOkenisation
-sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
-word_tokens = nltk.word_tokenize(raw)# converts to list of words
-
-# Preprocessing
+# nltk.download('popular', quiet=True)
+# nltk.download('punkt')
+# nltk.download('wordnet')
 lemmer = WordNetLemmatizer()
-def LemTokens(tokens):
-    return [lemmer.lemmatize(token) for token in tokens]
-remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 
-def LemNormalize(text):
-    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+nlp = sp.load('en_core_web_sm')
 
-# Keyword Matching
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
-GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+pwd = os.getcwd()
+fileDir = pwd+'\\Data\\Faculty\\CSVs'
 
-def greeting(sentence):
-    """If user's input is a greeting, return a greeting response"""
-    for word in sentence.split():
-        if word.lower() in GREETING_INPUTS:
-            return random.choice(GREETING_RESPONSES)
+e = pd.read_csv('Events\events.csv')
+eNames = list(e['Name'])
+eTimes = list(e['Time'])
+
+arch = pd.read_csv(fileDir+'\Archdata.csv')
+archNames = list(arch['Name'])
+archDesg = list(arch['Designation'])
+archEmail = list(arch['E-mail'])
+
+bt = pd.read_csv(fileDir+'\Btdata.csv')
+btNames = list(arch['Name'])
+btDesg = list(arch['Designation'])
+btEmail = list(arch['E-mail'])
+
+cv = pd.read_csv(fileDir+'\civildata.csv')
+cvNames = list(arch['Name'])
+cvDesg = list(arch['Designation'])
+cvEmail = list(arch['E-mail'])
+
+cs = pd.read_csv(fileDir+'\csedata.csv')
+csNames = list(arch['Name'])
+csDesg = list(arch['Designation'])
+csEmail = list(arch['E-mail'])
+
+ec = pd.read_csv(fileDir+'\ecedata.csv')
+ecNames = list(arch['Name'])
+ecDesg = list(arch['Designation'])
+ecEmail = list(arch['E-mail'])
+
+ee = pd.read_csv(fileDir+'\eeedata.csv')
+eeNames = list(arch['Name'])
+eeDesg = list(arch['Designation'])
+eeEmail = list(arch['E-mail'])
+
+mech = pd.read_csv(fileDir+'\mechdata.csv')
+mechNames = list(arch['Name'])
+mechDesg = list(arch['Designation'])
+mechEmail = list(arch['E-mail'])
+
+sh = pd.read_csv(fileDir+'\s&hdata.csv')
+shNames = list(arch['Name'])
+shDesg = list(arch['Designation'])
+shEmail = list(arch['E-mail'])
+
+cal = pd.read_csv('Calendar\calendar.csv')
+calDate = list(cal['Date'])
+hol = list(cal['Holiday'])
+res = list(cal['Result'])
+holIdx = []
+for i in range(len(hol)):
+    if hol[i] == 'Yes':
+        holIdx.append(i)
+resIdx = res.index('Yes')
+
+hi = ['Hi there!', 'Hey!', 'Hello! How can i help you?',
+      'Sup', 'What can I do for you?']
+bye = ['Bye!', 'See you!', 'Goodbye!', 'Have a nice day!']
 
 
-# Generating response
-def response(user_response):
-    response=''
-    sent_tokens.append(user_response)
-    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
-    tfidf = TfidfVec.fit_transform(sent_tokens)
-    vals = cosine_similarity(tfidf[-1], tfidf)
-    idx=vals.argsort()[0][-2]
-    flat = vals.flatten()
-    flat.sort()
-    req_tfidf = flat[-2]
-    if(req_tfidf==0):
-        response=response+"I am sorry! I don't understand you"
-        return response
-    else:
-        response = response+sent_tokens[idx]
-        return response
+def hello():
+    print(random.choice(hi))
 
 
-flag=True
-print(">>Hi there! If you want to exit, type Bye!")
-while(flag==True):
-    user_response = input()
-    user_response=user_response.lower()
-    if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
-            flag=False
-            print(">> You're welcome.")
-        else:
-            if(greeting(user_response)!=None):
-                print(">> "+greeting(user_response))
+def bye():
+    print(random.choice(bye))
+
+
+hello()
+userResponse = input('Type Bye to exit\n')
+userResponse = nlp(userResponse.lower())
+words = [x.lemma_ for x in userResponse]
+
+while('bye' not in words):
+    if 'sup' in words or 'hello' in words or 'hey' in words or 'hi' in words:
+        hello()
+
+    elif 'be' in words or 'what' in words or 'which' in words or 'when' in words:
+        # questions related to events occuring in college
+        if 'event' in words or 'activity' in words or 'occur' in words:
+            if eNames:
+                print("The following events are occuring: ")
+                for i in range(len(eNames)):
+                    print(eNames[i], 'occuring on', eTimes[i])
             else:
-                print(">> ",end="")
-                print(response(user_response))
-                sent_tokens.remove(user_response)
+                print('No events are going on currently.')
+
+        elif 'holiday' in words:  # questions related to holidays
+            if 'tomorrow' in words:
+                tomDateIndex = calDate.index(
+                    str(datetime.date.today()+datetime.timedelta(days=1)))
+                if hol[tomDateIndex] == 'Yes':
+                    print('Yes, tomorrow is a holiday!')
+                else:
+                    print('Sorry, tomorrow is not a holiday')
+            else:
+                print('The following dates are upcoming holidays:')
+                for i in holIdx:
+                    print(calDate[i])
+
+        elif 'result' in words:  # questions related to results release
+            if 'tomorrow' in words:
+                tomDateIndex = calDate.index(
+                    str(datetime.date.today()+datetime.timedelta(days=1)))
+                if res[tomDateIndex] == 'Yes':
+                    print('Yes, tomorrow the results will be released!')
+                else:
+                    print(
+                        'The results will not be released tomorrow. They will be released on: ', calDate[resIdx])
+            else:
+                print('The results will be released on: ', calDate[resIdx])
+
     else:
-        flag=False
-        print(">> Bye! take care..")    
+        print("I'm sorry, I didn't understand that.")
+    userResponse = input()
+    userResponse = nlp(userResponse.lower())
+    words = [x.lemma_ for x in userResponse]
+    # print(words)
+
+bye()
